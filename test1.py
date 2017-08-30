4d10c4f756cc1c871b133b234a3eb14f319705a4
MiracleBerserker <873144200@qq.com>
2017-08-29 19:50:29 -0700

    shell commit

---
+++ test1.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+#test python
+
+'''
+
+ssss
+dddddd
+
+'''
+
+
+print("===============================test start===============================");
+
+'''url="cherry";
+print("hello",url);
+print("hello%s"%url);
+
+
+num=input("please input your num:");
+print("your num is :",num);
+
+
+num1=input("your first num:");
+num2=input("your second num:");
+num1=int(num1);
+num2=int(num2);
+num3=num1+num2;
+print("num1+num2=%i"%num3);
+print(type(num1));
+'''
+age=input("your age:");
+age=int(age);
+if age<18:
+   print("you is so yuang");
+else:
+    print("you is so old"); 
+
+
